4a8eade7415fdc011de12bc183aca16bb9c2bd3a
subhojit <subhojitroy00012@gmail.com>
2023-09-18 09:22:47 +0530

    small changes

--- script.js
+++ script.js
@@ -35,30 +35,7 @@ async function checkWeather(city) {
     document.querySelector(".weather").style.display = "block";
     document.querySelector(".error").style.display = "none";
   }
-
-  var data = await response.json();
-
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-
-  if (data.weather[0].main == "Cloud") {
-    weatherIcon.src = "../images/clouds.png";
-  } else if (data.weather[0].main == "Clear") {
-    weatherIcon.src = "../images/clear.png";
-  } else if (data.weather[0].main == "Rain") {
-    weatherIcon.src = "../images/rain.png";
-  } else if (data.weather[0].main == "Drizzle") {
-    weatherIcon.src = "../images/drizzle.png";
-  } else if (data.weather[0].main == "Mist") {
-    weatherIcon.src = "../images/mist.png";
-  }
-
-  document.querySelector(".weather").style.display = "block";
 }
-
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
-// checkWeather();
